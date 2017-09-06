88c8bfd66122a92a48f6ba2eff0e6e6301a2a98f
sbarnwell88 <susy6688@gmail.com>
2017-09-06 10:59:51 -0400

    added new and create routes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -9,5 +9,13 @@ class CreaturesController < ApplicationController
     end
 
     def create 
+        @creature = Creature.create!(creature_params)
+
+        redirect_to creatures_path(@creature)
+    end
+
+    private 
+    def creature_params
+        params.require(:creature).permit(:name, :description)
     end
 end
