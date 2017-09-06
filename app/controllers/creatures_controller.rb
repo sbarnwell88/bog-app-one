6dd7403fd1a0bc12f9c00596c555b4b6730171b1
sbarnwell88 <susy6688@gmail.com>
2017-09-06 10:51:26 -0400

    added form

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,4 +3,11 @@ class CreaturesController < ApplicationController
     def index
         @creatures = Creature.all 
     end
+
+    def new
+        @creature = Creature.new
+    end
+
+    def create 
+    end
 end
