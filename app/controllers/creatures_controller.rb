cb50e0c43652555ba4aad5d87daf7ae486f05041
sbarnwell88 <susy6688@gmail.com>
2017-09-06 12:24:18 -0400

    finished all functionality

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -18,6 +18,24 @@ class CreaturesController < ApplicationController
         @creature = Creature.find params[:id]
     end
 
+    def edit
+        @creature = Creature.find params[:id]
+    end
+
+    def update
+        @creature = Creature.find params[:id]
+        @creature.update(creature_params)
+
+        redirect_to creature_path(@creature)
+    end
+
+    def destroy
+        @creature = Creature.find params[:id]
+        @creature.destroy
+
+        redirect_to creatures_path
+    end
+
     private 
     def creature_params
         params.require(:creature).permit(:name, :description)
